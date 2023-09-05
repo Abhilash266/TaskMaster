c214845c00e2e89a4d10dc10a7325eed73eb4441
Abhilash266 <74292017+Abhilash266@users.noreply.github.com>
2023-09-05 12:03:34 -0700

    Update home.js

--- src/Backend/home.js
+++ src/Backend/home.js
@@ -71,7 +71,15 @@ app.get('/api/getUserData/:id',(req, res) => {
   res.send(data)
 })
 
+const path = require('path');
 
+const __dirname1 = path.resolve();
+
+app.use(express.static(path.join(__dirname1,"/src/Frontend/build")));
+
+app.get("*",(req, res) => {
+    res.sendFile(path.resolve(__dirname1, "src", "Frontend", "build", "index.html"))
+})
 
 
 
