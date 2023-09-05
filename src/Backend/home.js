edc2864a2652d08cc7476aade28b3af02e13b1a3
Abhilash266 <74292017+Abhilash266@users.noreply.github.com>
2023-09-05 12:06:26 -0700

    Update home.js

--- src/Backend/home.js
+++ src/Backend/home.js
@@ -75,10 +75,10 @@ const path = require('path');
 
 const __dirname1 = path.resolve();
 
-app.use(express.static(path.join(__dirname1,"/src/Frontend/build")));
+app.use(express.static(path.join(__dirname1,"/build")));
 
 app.get("*",(req, res) => {
-    res.sendFile(path.resolve(__dirname1, "src", "Frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname1, "build", "index.html"))
 })
 
 
