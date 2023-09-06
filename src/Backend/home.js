3effd1ec4313a475a064ad58a399144e5a04ab35
Abhilash266 <74292017+Abhilash266@users.noreply.github.com>
2023-09-05 17:07:41 -0700

    home.js updated

--- src/Backend/home.js
+++ src/Backend/home.js
@@ -74,7 +74,7 @@ app.get('/api/getUserData/:id',(req, res) => {
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
